527ed5fb99484e7f308c06b069fb10ed3ad29529
April Reitan <45362322+areitan@users.noreply.github.com>
2022-03-25 11:54:44 -0700

    testing basic functions for setting classes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,26 @@
-var am9 = $('#am9');
-var row9 = $('#row9');
-
 // Capture today's date
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 console.log(today)
 
-var currentHour = moment().format("hA");
+// Finds current hour
+var currentHour = moment().format("h");
 console.log(currentHour)
 
-if (currentHour == am9.text)
+var am9 = parseInt($('#am9'));
+var row9 = $('#row9');
 
-// adds class present
-row9.addClass('present');
+
+if (currentHour > $(am9).text()) {
+    // adds class present
+    row9.addClass('past');
+    // else if()
+}
+
+var am10 = parseInt($('#am10'));
+var row10 = $('#row10');
+
+if (currentHour > $(am10).text()) {
+    // adds class present
+    row10.addClass('past');
+}
